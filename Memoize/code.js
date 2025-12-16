f23a9e7152742b73a9cfc326ae02da6a14b3460b
Edwin <princenyarkoedwin@gmail.com>
2025-12-16 11:12:37 +0000

    Add memoize function for caching results
    Implement memoization function to cache results.

---
+++ Memoize/code.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = {};
+    
+    return function(...args) {
+    
+        const key = JSON.stringify(...args);
+        if(cache[key] != undefined){
+            return cache[key];
+        }
+        else{
+            const value = fn(...args);
+            cache[key] = value;
+            return value;
+        }
+        
+
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
